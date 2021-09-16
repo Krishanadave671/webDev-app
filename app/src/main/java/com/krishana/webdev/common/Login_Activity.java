aa94681290cb79655b8fbe66b8369719fd90844a
Krishana Dave <krishnadave671@gmail.com>
2021-09-16 17:35:54 +0530

    login and signup page created

--- app/src/main/java/com/krishana/webdev/common/Login_Activity.java
+++ app/src/main/java/com/krishana/webdev/common/Login_Activity.java
@@ -1,15 +1,16 @@
-package com.krishana.webdev;
+package com.krishana.webdev.common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.krishana.webdev.R;
+
+public class Login_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
 }
