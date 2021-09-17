ce12931274b3a7cf54dc1aa675126a1df4a14cc0
Tanish <stanish.mi@gmail.com>
2021-09-17 03:58:47 -0700

    nav drawer and bottom nav bar (layout)

---
+++ app/src/main/java/com/krishana/webdev/activity_bottomnavigation.java
@@ -0,0 +1,14 @@
+package com.krishana.webdev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_bottomnavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottomnavigation);
+    }
+}
